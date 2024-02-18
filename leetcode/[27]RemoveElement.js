5b872820d32c6e40248d0f527c29e3195a4025c4
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2024-02-18 16:10:36 +0900

    [leetcode][fix]day5

--- leetcode/[27]RemoveElement.js
+++ leetcode/[27]RemoveElement.js
@@ -1,15 +1,33 @@
+// SOLUTION 1
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+*/
+var removeElement = function(nums, val) {
+    const newNums = nums.filter((el) => el !== val);
+    for (let i = 0; i < newNums.length; i++) {
+        nums[i] = newNums[i];
+    }
+    
+    console.log(newNums);
+    console.log(nums);
+    return nums;
+};
+
+// SOLUTION 2
 /**
  * @param {number[]} nums
  * @param {number} val
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    const newNums = nums.filter((el) => el !== val);
-    for (let i = 0; i < newNums.length; i++) {
-        nums[i] = newNums[i];
+    let index = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] != val) {
+            nums[index] = nums[i];
+            index++;
+        }
     }
-
-    console.log(newNums);
-    console.log(nums);
-    return nums;
-};
+    return index;
+}; 
