a6427b61fc9245b3d2387796410fd0b2be2eaf40
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2024-04-06 23:48:52 +0900

    [leetcode]day54

---
+++ leetcode/[2796]RepeatString.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} times
+ * @return {string}
+ */
+String.prototype.replicate = function(times) {
+    if (times === 0) {
+        return "";
+    }
+    //Here "this" refers to the string object on which the method is called
+    return this + this.replicate(times - 1);
+};
