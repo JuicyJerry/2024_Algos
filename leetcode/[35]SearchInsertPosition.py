305272d9799b9c82fc07fa9daba1375fbc97b671
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2024-05-25 23:47:00 +0900

    today algo

---
+++ leetcode/[35]SearchInsertPosition.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+        return len(nums)
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
+        return left
