856dbab4e8942d667ae2d8476e846fa80329c2fa
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2024-07-21 23:19:21 +0900

    today algos

---
+++ leetcode/[p]리스트_자르기.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(int n, vector<int> slicer, vector<int> num_list) {
+    vector<int> ans;
+    switch(n) {
+        case 1:
+            for (int i = 0; i <= slicer[1]; i++) {
+                ans.push_back(num_list[i]);
+            }
+            break;
+        case 2:
+            for (int i = slicer[0]; i < size(num_list); i++) {
+                ans.push_back(num_list[i]);
+            }
+            break;
+        case 3:
+            for (int i = slicer[0]; i <= slicer[1]; i++) {
+                ans.push_back(num_list[i]);
+            }
+            break;
+        case 4:
+            for (int i = slicer[0]; i <= slicer[1]; i+=slicer[2]) {
+                ans.push_back(num_list[i]);
+            }
+            break;
+    }
+    
+    return ans;
+}
